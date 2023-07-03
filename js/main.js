b0284a9261a2389e3c8ba8e658c2de5f6740b4e6
vuyghu2020 <111427114+vuyghu2020@users.noreply.github.com>
2023-07-04 00:30:11 +0200

    e

---
+++ js/main.js
@@ -0,0 +1,54 @@
+console.clear()
+
+const ed = document.createElement("div")
+ed.id = "blog"
+
+blogs = [
+
+{"title":"Kampanjfilm för Cyan, Spring green, påsk gröna eller vad fan vi nu heter.",
+"image":"cyan kampanjfilm.webp",
+"url":"https://www.youtube.com/watch?v=BR1pKZsegvQ"},
+
+{"title":"Cyan team reagerar på reddit cows",
+"image":"redit cows reagering.jpg",
+"url":"e"},
+
+]
+
+
+
+
+
+blogs.forEach(blog => {
+    
+    
+
+    console.log(blog.title);
+    console.log(blog.image);
+    console.log(blog.url);
+
+    function o() {
+        open(blog.url);
+    }
+
+    const e = document.createElement("div")
+    e.onclick = o
+    const img = document.createElement("img")
+    img.src = `assets/blogs/${blog.image}`
+    const blog_title = document.createElement("p")
+    blog_title.innerText = blog.title
+    blog_title.classList.add("blog_titles")
+    e.classList.add("blogs")
+
+    e.appendChild(img)
+    e.appendChild(blog_title)
+    ed.appendChild(e)
+        
+
+    
+    
+    
+});
+
+
+document.body.appendChild(ed)
